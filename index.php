955951d523839ff64a7a331993eec3fc11b2d647
Shreekar Kolanu <43133388+Skillz619@users.noreply.github.com>
2024-02-15 18:24:20 -0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+// index.php
+
+// Some PHP code
+$message = "Hello, World! This is a message from PHP.";
+
+// HTML content starts here
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +14,6 @@
     <title>Sample PHP Webpage</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
-    
-    <?php
-        // PHP code can go here
-        $message = "This is a message from PHP!";
-        echo "<p>$message</p>";
-    ?>
+    <h1><?php echo $message; ?></h1>
 </body>
 </html>
